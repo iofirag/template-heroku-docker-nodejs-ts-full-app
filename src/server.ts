59c8d5a71b5b811e7a1dbe1e9cb52a218e2230e9
iofirag <iofirag@gmail.com>
2020-04-28 13:27:34 +0300

    add ip warning message

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import app from "./app";
 
+const notifyMessage: String = `
+  NOTE: 
+  - if you are using Docker-Toolbox you can see your ip address in Docker-Terminal by running: "$(docker-machine ip)"
+  (Docker toolbox doesn't map ports to localhost. It maps it to the Docker VM IP's)
+  
+  - if you are using Docker-For-Windows your ip will be just normal as localhost / 127.0.0.1`
+
 class Server {
   constructor() {
     this.configureServer();
@@ -8,22 +15,22 @@ class Server {
 
   private configureServer(): void {
     // Configure server host + port
-    app.set("host", process.env.HOST || "localhost");
+    app.set("host", process.env.HOST || '<your-ip>');
     app.set("port", process.env.PORT || 3000);
   }
 
   private createServer(): void {
     app.listen(app.get("port"), () => {
+      // Note: 
+      
       console.log(
         `🌎 Typescript app listening on
         ${app.get("host")}:${app.get("port")}
-          Open up https://${app.get("host")}:${app.get(
-          "port"
-        )}/ in your browser.`
+        Open up https://${app.get("host")}:${app.get("port")}/ in your browser.
+        ${notifyMessage}`
       );
     });
   }
 }
 
 new Server();
-
